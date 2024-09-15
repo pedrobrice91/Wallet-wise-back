f6f41a1a6ea548e91d2a304b300625181dc9cf1b
marcourrutia <m.urrutia1991@gmail.com>
2024-09-15 20:50:39 -0300

    new user_add

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from flask import Flask, jsonify, request
 from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required
+from flask_bcrypt import Bcrypt
 from models import db, User
+import re
 
 app = Flask(__name__) #__main__
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///wallet-wise.db"
+app.config["JWT_SECRET_KEY"] = "secret_key"
+app.config["SECRET_KEY"] = "contrasena-super-segura"
+JWTManager(app)
+bcrypt = Bcrypt(app)
 db.init_app(app)
 Migrate(app, db)
 
@@ -17,9 +24,38 @@ def home():
 def user():
     data = request.get_json()
     user = User()
-    user.name = data["name"]
-    user.email = data["email"]
-    user.password = data["password"]
+    name = data["name"]
+    email = data["email"]
+    password = data["password"]
+
+    if email is not None:
+        email_re = r"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$"
+        if re.match(email_re, email):
+            user.email = email
+        else:
+            return jsonify({
+                "msg": "Correo con formato invalido"
+            }), 400
+    else:
+        return jsonify({
+            "msg": "El email es requerido"
+        }), 400
+    
+    if password is not None:
+        password_re = r"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$"
+        if re.match(password_re, password):
+            password_hash = bcrypt.generate_password_hash(password)
+            user.password = password_hash
+        else:
+            return jsonify({
+                "msg": "Formato de contraseña no valido"
+            }), 400
+    else:
+        return jsonify({
+            "msg": "La contraseña es requerida"
+        }), 400
+    
+    user.name = name
 
     db.session.add(user)
     db.session.commit()
